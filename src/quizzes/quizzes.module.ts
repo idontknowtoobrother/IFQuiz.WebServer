ec5fe2f5b32552c17329341390c1696a8e9aed13
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-01 19:10:01 +0700

    done: simple service get all quizzes

--- src/quizzes/quizzes.module.ts
+++ src/quizzes/quizzes.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { QuizzesController } from './quizzes.controller';
 import { QuizzesService } from './quizzes.service';
+import { QuizzesSchema } from './schemas/quizzes.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: "Quizzes", schema: QuizzesSchema }
+    ])
+  ],
   controllers: [QuizzesController],
   providers: [QuizzesService]
 })
--- src/quizzes/quizzes.service.ts
+++ src/quizzes/quizzes.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Quizzes } from './schemas/quizzes.schema';
 
 @Injectable()
-export class QuizzesService {}
+export class QuizzesService {
+    constructor(
+        @InjectModel(Quizzes.name)
+        private quizzesModel: Model<Quizzes>
+    ){}
+
+    // get all quizzes
+    async findAll() : Promise<Quizzes[]>{
+        const quizzes = this.quizzesModel.find()
+        return quizzes
+    }
+
+
+
+}
