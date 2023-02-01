9dadee6ef1580f771e98708f46074a0f30d12cfb
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-01 18:26:26 +0700

    create: jwt strategy

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { PassportStrategy } from "@nestjs/passport"
+import { Model } from "mongoose";
+import { Strategy, ExtractJwt } from "passport-jwt";
+import { User } from "./schemas/user.schemas";
+
+ 
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        @InjectModel(User.name)
+        private userModel : Model<User>
+    ){
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET
+        })
+    }
+
+    async validate(payload){
+        // get id from payload of token 
+        // cause we save id from login and logout to payload token
+        const { id } = payload
+
+        // find the user in database by id
+        const user = await this.userModel.findById(id)
+        
+        // if not found user by did not login yet or token expires
+        // it's will throw error UnAuth 
+        if(!user) {
+            throw new UnauthorizedException("Please login first !")
+        }
+
+        return user;
+    }
+}
