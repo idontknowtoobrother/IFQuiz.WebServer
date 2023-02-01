1398a3cdc6ea5817d6283b65302ddb1752662c80
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-01 15:54:11 +0700

    add controller,remove users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/ifquiz'), AuthModule, UsersModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/ifquiz'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
