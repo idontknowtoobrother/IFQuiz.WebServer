6cfb0bdb5aa0b453bc0110e60396d1dca6e18ba8
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-01 15:47:01 +0700

    init module and service of users and auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/ifquiz')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/ifquiz'), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
