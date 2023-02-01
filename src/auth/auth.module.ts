19ef10df79d17b5187fadbb6e61502ce75590fcb
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-02-01 16:24:03 +0700

    imports: passportModule, JwtModule with ConfigService(dotenv) and UserSchemas

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserSchema } from './schemas/user.schemas';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    PassportModule.register({ 
+      defaultStrategy: 'jwt'
+    }),
+    JwtModule.registerAsync({
+      inject:[ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: {
+            expiresIn: config.get<string | number>('JWT_EXPIRE')
+          },
+        }
+      }
+    }),
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema}
+    ])
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
