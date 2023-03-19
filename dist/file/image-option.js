57c76470fb3c5acd32221259d4f8ab8972909be3
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-03-19 21:44:13 +0700

    ignore node_modules

--- dist/file/image-option.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.imageOptions = void 0;
-exports.imageOptions = {
-    limits: { fileSize: 1024 * 1024 },
-};
-//# sourceMappingURL=image-option.js.map
