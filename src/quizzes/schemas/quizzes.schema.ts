d26039a73e7084c343bb82da98698b9268c563c8
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-17 03:51:16 +0700

    remove unuse category

--- src/quizzes/schemas/quizzes.schema.ts
+++ src/quizzes/schemas/quizzes.schema.ts
@@ -3,17 +3,6 @@ import mongoose from "mongoose";
 import { User } from "../../auth/schemas/user.schema";
 import { QuestionDto } from "../dto/question.dto";
 
-
-export enum Category {
-    MATH = 'Mathematics',
-    SCIENCE = 'Science',
-    ENG = 'English Language',
-    COMSCI = 'Computer Science',
-    PE = 'Physical Education',
-    ART = 'Creative Arts',
-    LANGUAGE = 'World Language',
-}
-
 @Schema({ timestamps: true })
 export class Quizzes {
 
@@ -23,8 +12,6 @@ export class Quizzes {
     @Prop({ required: true})
     description : string
 
-    @Prop({required: true})
-    category : Category
 
     @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
     user: User
