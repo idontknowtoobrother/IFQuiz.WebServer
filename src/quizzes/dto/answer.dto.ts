67e4ca03cb878a035c4073878a5f0eed9579975c
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-17 03:38:01 +0700

    remove select answer

--- src/quizzes/dto/answer.dto.ts
+++ src/quizzes/dto/answer.dto.ts
@@ -33,11 +33,6 @@ export class SingleAnswer extends Answers {
 
     @Prop({required: true})
     readonly type : AnswerTypes.SINGLE_CHOICE
-
-    @Prop({required: true})
-    @IsNotEmpty()
-    @IsArray()
-    readonly selectAnswers: Answer[]
     
     @Prop({required: true})
     @IsNumber()
@@ -49,11 +44,6 @@ export class MultipleAnswer extends Answers {
 
     @Prop({required: true})
     readonly type : AnswerTypes.MULTIPLE_CHOICE
-    
-    @Prop({required: true})
-    @IsNotEmpty()
-    @IsArray()
-    readonly selectAnswers: Answer[]
 
     @Prop({required: true})
     @IsArray()
