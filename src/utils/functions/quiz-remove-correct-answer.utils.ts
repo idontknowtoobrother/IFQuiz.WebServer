c8a710dd6290846b9e3168d84758c7b6752be063
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-20 07:58:28 +0700

    added-utils: initial take quiz and get quiz without answer

--- src/utils/functions/quiz-remove-correct-answer.utils.ts
+++ src/utils/functions/quiz-remove-correct-answer.utils.ts
@@ -1,3 +1,5 @@
+import { RunningQuizzes } from "src/quizzes/schemas/running.quizzes.schema"
+
 export function getQuizzesWithOutCorrectAnswer(quizzes: any){
     let quizzesFinal = quizzes
 
@@ -16,6 +18,52 @@ export function getQuizzesWithOutCorrectAnswer(quizzes: any){
 
 export function getQuizWithOutCorrectAnswer(quiz: any){
     let quizFinal = quiz
+
+    for(let question of quizFinal.questions){
+        for(let ans of question.answer){
+            delete ans?.checked
+            delete ans?.type
+            delete ans?.matchString
+        }
+    }
+
+    return quizFinal
+}
+
+export function initialTakeQuizAnswer(quiz: any){
+    const quizFinal = quiz
+    let id = 0
+    for(let question of quizFinal.questions){
+        
+        if (question.type === 'single-choice'){
+            quizFinal.answers.push({
+                questionId: id,
+                selectedId: null 
+            })
+        }
+        
+        if(question.type === 'multiple-choice'){
+            quizFinal.answers.push({
+                questionId: id,
+                selectedIds: [] 
+            })
+        }
+
+        if(question.type === 'fill-choice'){
+            quizFinal.answers.push({
+                questionId: id,
+                matchString: null
+            })
+        }
+     
+        id++
+    }
+
+    return quizFinal
+}
+
+export function getTakeQuizWithOutAnswer(quiz: any) {
+    const quizFinal = quiz
 
     for(let question of quizFinal.questions){
         for(let ans of question.answer){
