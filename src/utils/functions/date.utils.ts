2d8ee14cbc1aeca7d8a6e4348d8e5f14fb6a121b
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2023-06-21 07:37:24 +0700

    added: function for get date with diff time cause we use at 2 position on deployed and update answer

--- src/utils/functions/date.utils.ts
+++ src/utils/functions/date.utils.ts
@@ -1,3 +1,12 @@
+import { Duration } from "src/quizzes/dto/duration.dto";
+
 export function isExpired(expiredDate: Date): boolean {
    return new Date() > expiredDate
+}
+
+export function getDateWithDuration(duration: Duration, addDiffMinutes?: number): Date {
+
+   const durationMilliseconds = ((duration.hours * 60 * 60 * 1000) + (duration.minutes * 60 * 1000)) + ((addDiffMinutes || 0) * 60 * 1000);
+
+   return new Date(Date.now() + durationMilliseconds)
 }
